04a4299f82416264072c3001dfbb5c0a4ddd2c73
Rob <robert.litzke@gmail.com>
2013-04-25 22:26:35 -0400

    Add comments

--- simple_genetic_algorithm.py
+++ simple_genetic_algorithm.py
@@ -1,59 +1,74 @@
 from random import randint, random
-from math import log
-operations = { 10:'-', 11:'+', 12:'/', 13:'*' }
-MAX_SIZE=15
+import pdb
+#These 'constants' will be treated as operations
+operations = { 10:'-', 11:'+', 12:'/', 13:'*', 14:'%' }
+#Nulls are handled ok (15 will be invalid)
+MAX_NUM=15
+#The highest digit allowed
+MAX_CONST=9
 
+#Replace gene numbers with constants
 def set_operations(val):
-	if val<=9:
+	if val<=MAX_CONST:
 		return val
 	if operations.has_key(val):
 		return operations[val]
 	return None
 
-def build_expr(genes):
-	nums = [set_operations(x) for x in genes]
-	nums = [i for i in nums if i!=None]
-	prev = None
+#Turns a genome into an expression
+def build_expr(genome):
+	nodes = [set_operations(gene) for gene in genome]
+	no_null = [node for node in nodes if node!=None]
 	out = []
-	for item in nums:
-		if type(item) != type(prev):
-			out.append(item)
-		prev = item
+	prev = None
+	#Strip out repeats up-front, eg: '6 6 +' or '* * 9'
+	for node in no_null:
+		if type(prev) != type(node):
+			out.append(node)
+		prev = node
+	#Return a string that can be evaluated
 	return ' '.join([str(x) for x in out])
 
+
+#Try/Catch to determine if the expression will live
 def is_viable(genome):
 	try:
-		return eval(build_expr(genome)) != None
+		expr = build_expr(genome)
+		return eval(expr) != None
 	except:
 		return False
 
+#Generate a completely random genome
 def generate(genome_length):
-	return [randint(0,MAX_SIZE) for x in xrange(0,genome_length)]
+	y=[randint(0,MAX_NUM) for x in xrange(genome_length)]
+	#pdb.set_trace()
+	return y
 
-def print_gen(gen_count,genomes,target):
-	print '\nGeneration {0} Avg Fitness: {1:.3f}'.format(gen_count,sum([fitness(x,target) for x in genomes])/float(len(genomes)))
-	for genome in genomes:
-		expr = build_expr(genome)
-		print 'Expr: {0:<45} Result: {1:<5} Fit: {2:.3f}'.format(expr,eval(expr),fitness(genome,target))
-
-def build_gen(genome_length,pop_size):
+#Initialiazes a full generation of viable genomes
+def initialize_generation(genome_length,pop_size):
 	result = []
 	while len(result) < pop_size:
 		genome = generate(genome_length)
+		print genome
 		if is_viable(genome):
 			result.append(genome)
 	return result
 
+#Determines teh fitness of genes. The close to the target, the higher the result
 def fitness(genes,target):
 	expr = build_expr(genes)
+	#1+abs() to prevent divide-by-0
 	return 100/(1+abs(float((target-eval(expr)))))
 
+#Who survives from each generation? Play a game of roulette
 def roulette(population,count,target):
 	out = []
 	for n in xrange(count):
+		#First, allocate a distribution from 0-1.0
 		fitnesses = [fitness(x,target) for x in population]
 		rel_fitness = [f/float(sum(fitnesses)) for f in fitnesses]
 		probs = [sum(rel_fitness[:i+1]) for i in range(len(fitnesses))]
+		#Generate a random number and see where it falls in the population
 		r = random()
 		for (i, individual) in enumerate(population):
 			if r <= probs[i]:
@@ -62,10 +77,12 @@ def roulette(population,count,target):
 				break
 	return out
 
-def mutate(genes,rate):
-	out = [(gene if random()>rate else randint(0,MAX_SIZE)) for gene in genes]
+#Mutate rate percent of genes in a genome
+def mutate(genome,rate):
+	out = [(gene if random()>rate else randint(0,MAX_NUM)) for gene in genome]
 	return out
 
+#Mate two parents. Single pivot, random parent first, random choice from start/end
 def mate(parents):
 	rand = randint(1,len(parents[0])-1)
 	choice = randint(0,1)
@@ -75,22 +92,42 @@ def mate(parents):
 		child = parents[choice][rand:]+parents[choice^1][:rand]
 	return child
 
+#Rebuild population after culling
 def regenerate(population,target_size,mutation_rate):
 	out = list(population)
 	while len(out)<target_size: 
+		#Choose random parents from survivors
 		parents = [population[randint(0,len(population)-1)],population[randint(0,len(population)-1)]]
+		#Crossover and mutate
 		child = mutate(mate(parents),mutation_rate)
+		#If it survives, keep it
 		if is_viable(child):
 			out.append(child)
 	return out
 
-def harness(runs,genome_length,pop_size,survivor_count,mutation,target):
-	population = build_gen(genome_length,pop_size)
+#Pretty-prints a set of genomes
+def print_gen(gen_count,genomes,target):
+	print '\nGeneration {0} Avg Fitness: {1:.3f}'.format(gen_count,sum([fitness(x,target) for x in genomes])/float(len(genomes)))
+	for genome in genomes:
+		expr = build_expr(genome)
+		print 'Expr: {0:<45} Result: {1:<5} Fit: {2:.3f}'.format(expr,eval(expr),fitness(genome,target))
+
+'''
+	Harness for running the genetic algorithm
+	* runs - the max number of generations to run for
+	* genome_length - the number of genes (not all will be expressed)
+	* pop_size - the number of genomes in the population
+	* survivor_count - how many live?
+	* mutation_rate - what percent of genes mutate?
+	* target - what should the expression evaluate to?
+'''
+def harness(runs,genome_length,pop_size,survivor_count,mutation_rate,target):
+	population = initialize_generation(genome_length,pop_size)		#Initialize the herd
 	for i in xrange(runs):
-		survivors = roulette(population,survivor_count,target)
-		print_gen(i,survivors,target)
-		population = regenerate(survivors,pop_size,mutation)
-		a = [x for x in population if eval(build_expr(x))==target]
+		survivors = roulette(population,survivor_count,target) 		#Cull the herd!
+		print_gen(i,survivors,target)								#Show the slaughter
+		population = regenerate(survivors,pop_size,mutation_rate)	#Reproduce
+		a = [x for x in population if eval(build_expr(x))==target] 	#On perfection, terminate
 		if len(a)>0:
 			break
-	print_gen('Complete',population,target)
+	print_gen('Complete',population,target) 						#Print the final generation
